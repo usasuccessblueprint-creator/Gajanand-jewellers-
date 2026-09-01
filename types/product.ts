1f503d01b30626b4bf2cad02b78b13d58b9a0fe5
usasuccessblueprint-creator <usasuccessblueprint@gmail.com>
2026-09-01 16:01:29 +0530

    Add product type definitions

---
+++ types/product.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  category: string;
+  stock: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface CartItem extends Product {
+  quantity: number;
+}
